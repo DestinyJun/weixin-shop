c21102b0c6b2031dac0340b996798c231b15c49d
文君 <429771890@qq.com>
2019-03-11 01:02:17 +0800

    操作日志： （1）打包修复
           ————————生活需要方向和激情，希望你明白——————

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/tab/home', pathMatch: 'full'},
+  {path: '', redirectTo: '/registered', pathMatch: 'full'},
   {path: 'registered', loadChildren: './registered/registered.module#RegisteredModule'},
   {path: 'tab', loadChildren: './tab/tab.module#TabModule'},
   {path: 'order', loadChildren: './order/order.module#OrderModule'},
