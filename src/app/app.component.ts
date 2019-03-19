fdbae74ebe220fdfe078d3bffd38668f63060051
DestinyJun <429771890@qq.com>
2019-03-19 12:06:20 +0800

    操作日志： （1）等待服务器修复
    -------------------生活需要目标和方向，完美度过每一天-----------------

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {NavigationEnd, Router} from '@angular/router';
 import {environment} from '../environments/environment';
 import {ToastComponent, ToastService} from 'ngx-weui';
 import {GlobalService} from './common/services/global.service';
+import {Title} from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,32 +13,35 @@ import {GlobalService} from './common/services/global.service';
 export class AppComponent {
   // toast
   @ViewChild('statusRemindToast') statusRemindToast: ToastComponent;
-  title = 'weixin-shop';
-  msg: string;
+  public title = '魔药系统';
+  public msg: string;
   constructor(
     private router: Router,
     private globalSrv: GlobalService,
-    private toastSrv: ToastService
+    private toastSrv: ToastService,
+    private titleSrv: Title
   ) {
-    // this.toastSrv.loading('删除中...');
-    // this.toastSrv.hide();
-
-    // this.msg = val.message;
-    // this.onShow('statusRemind');
+    const title = {
+      '/registered': '请填写推荐人',
+    };
     console.log(environment.env);
-    router.events.subscribe(
+   /* router.events.subscribe(
       (event) => {
         if (event instanceof NavigationEnd) {
-          console.log(event.urlAfterRedirects);
+          titleSrv.setTitle(title[event.urlAfterRedirects]);
         }
       }
-    );
+    );*/
     globalSrv.remindEvent.subscribe(
       (val: any) => {
         // this.toastSrv.loading(val.msg);
         console.log(val);
       }
     );
+    // this.toastSrv.loading('删除中...');
+    // this.toastSrv.hide();
+    // this.msg = val.message;
+    // this.onShow('statusRemind');
   }
   public appRemindToast(type: string) {
     (<ToastComponent>this[`${type}Toast`]).onShow();
